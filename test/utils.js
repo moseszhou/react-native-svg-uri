543d40ef877ecae16c4bff4fbc08ac6069d5f39a
moseszhou <zhoujian@myai.tech>
2026-05-06 14:46:21 +0800

    feat: convert utils to TypeScript, migrate tests to Jest

--- test/utils.js
+++ test/utils.js
@@ -1,12 +1,10 @@
-import {expect} from 'chai';
-
-import {transformStyle, camelCase, removePixelsFromNodeValue, getEnabledAttributes} from '../utils';
+import {transformStyle, camelCase, removePixelsFromNodeValue, getEnabledAttributes} from '../src/utils';
 
 describe('transformStyle', () => {
   it('transforms style attribute', () => {
     expect(
       transformStyle({nodeName: 'style', nodeValue: 'fill:rgb(0,0,255);stroke:rgb(0,0,0)'})
-    ).to.deep.equal({
+    ).toEqual({
       fill: 'rgb(0,0,255)',
       stroke: 'rgb(0,0,0)',
     });
@@ -15,7 +13,7 @@ describe('transformStyle', () => {
   it('transforms style attribute with dash-case attribute', () => {
     expect(
       transformStyle({nodeName: 'style', nodeValue: 'stop-color:#ffffff'})
-    ).to.deep.equal({
+    ).toEqual({
       stopColor: '#ffffff',
     });
   });
@@ -23,20 +21,20 @@ describe('transformStyle', () => {
 
 describe('removePixelsFromNodeValue', () => {
   it('removes pixels from x, y, height and width attributes', () => {
-    expect(removePixelsFromNodeValue({nodeName: 'x', nodeValue: '2px'})).to.deep.equal({nodeName: 'x', nodeValue: '2'});
-    expect(removePixelsFromNodeValue({nodeName: 'y', nodeValue: '4px'})).to.deep.equal({nodeName: 'y', nodeValue: '4'});
-    expect(removePixelsFromNodeValue({nodeName: 'height', nodeValue: '65px'})).to.deep.equal({nodeName: 'height', nodeValue: '65'});
-    expect(removePixelsFromNodeValue({nodeName: 'width', nodeValue: '999px'})).to.deep.equal({nodeName: 'width', nodeValue: '999'});
+    expect(removePixelsFromNodeValue({nodeName: 'x', nodeValue: '2px'})).toEqual({nodeName: 'x', nodeValue: '2'});
+    expect(removePixelsFromNodeValue({nodeName: 'y', nodeValue: '4px'})).toEqual({nodeName: 'y', nodeValue: '4'});
+    expect(removePixelsFromNodeValue({nodeName: 'height', nodeValue: '65px'})).toEqual({nodeName: 'height', nodeValue: '65'});
+    expect(removePixelsFromNodeValue({nodeName: 'width', nodeValue: '999px'})).toEqual({nodeName: 'width', nodeValue: '999'});
   });
-})
+});
 
 describe('camelCase', () => {
   it('transforms two word attribute with dash', () => {
-    expect(camelCase('stop-color')).to.deep.equal('stopColor');
+    expect(camelCase('stop-color')).toEqual('stopColor');
   });
 
   it('does not do anything to string that is already camel cased', () => {
-    expect(camelCase('stopColor')).to.deep.equal('stopColor');
+    expect(camelCase('stopColor')).toEqual('stopColor');
   });
 });
 
@@ -45,14 +43,14 @@ describe('getEnabledAttributes', () => {
     const enabledAttributes = ['x', 'y', 'strokeOpacity'];
     const hasEnabledAttribute = getEnabledAttributes(enabledAttributes);
 
-    expect(hasEnabledAttribute({nodeName: 'x'})).to.deep.equal(true);
-    expect(hasEnabledAttribute({nodeName: 'stroke-opacity'})).to.deep.equal(true);
+    expect(hasEnabledAttribute({nodeName: 'x'})).toEqual(true);
+    expect(hasEnabledAttribute({nodeName: 'stroke-opacity'})).toEqual(true);
   });
 
   it('return false when nodeName is not found', () => {
     const enabledAttributes = ['width', 'height'];
     const hasEnabledAttribute = getEnabledAttributes(enabledAttributes);
 
-    expect(hasEnabledAttribute({nodeName: 'depth'})).to.deep.equal(false);
+    expect(hasEnabledAttribute({nodeName: 'depth'})).toEqual(false);
   });
 });
